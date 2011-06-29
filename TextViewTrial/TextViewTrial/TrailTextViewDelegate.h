e1e4718828a184b21b23072fed80eda1625cc744
kei10in <kei10in@gmail.com>
2011-06-29 21:08:59 +0900

    TrialTextView are added and connected as NSTextView delegation object. 

---
+++ TextViewTrial/TextViewTrial/TrailTextViewDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TrailTextViewDelegate.h
+//  TextViewTrial
+//
+//  Created by Keisuke Ijuin on 11/06/29.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TrailTextViewDelegate : NSObject <NSTextViewDelegate> {
+@private
+    
+}
+
+@end
---
+++ TextViewTrial/TextViewTrial/TrailTextViewDelegate.m
@@ -0,0 +1,30 @@
+//
+//  TrailTextViewDelegate.m
+//  TextViewTrial
+//
+//  Created by Keisuke Ijuin on 11/06/29.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TrailTextViewDelegate.h"
+
+
+@implementation TrailTextViewDelegate
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+
+@end
